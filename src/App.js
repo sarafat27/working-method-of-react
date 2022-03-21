a9b5e25723042e72502d6ed29e53b65101d0b804
sarafat27 <sarafat2769@gmail.com>
2022-03-21 16:30:54 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Device from './components/Device/Device';
+import Tablet from './components/Tablet/Tablet';
 import Watch from './components/Watch/Watch';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div style={{ border: '3px solid black', margin: '10px' }} className="App">
       <Device name="laptop" price="43000"></Device>
       <Watch></Watch>
+      <Tablet></Tablet>
     </div>
   );
 }
---
+++ src/components/Tablet/Tablet.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    // constructor(){
+
+    // }
+    // componentDidMount(){
+
+    // }
+    render() {
+        return (
+            <div>
+                <h1>This is tablet</h1>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
